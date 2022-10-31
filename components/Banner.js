0b1b88580dae77aff4396ecf66360eb101e209e8
koonzinc <koonz.eth@gmail.com>
2022-10-31 13:11:05 -0400

    finish banner

--- components/Banner.js
+++ components/Banner.js
@@ -1,16 +1,20 @@
 import Image from "next/image";
 
-
 const Banner = () => {
   return (
     <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
       <Image
-        src="https://links.papareact.com/0fm"
-        layout="fill"
-        className="object-cover"
+        src="https://images.squarespace-cdn.com/content/v1/572625bee707eb7c1fd6a5eb/1614096661575-7X13CDWQ0YLN6XYNHVEW/44-45_FINAL5_BIG.jpg?format=2500w"
+        width="1772"
+        height="1010"
+        className="w-full h-full object-cover opacity-50"
       />
-      <div className="absolute">
-          <p>Not sure where to go? Perfect.</p>
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
+          I'm flexible
+        </button>
       </div>
     </div>
   );
