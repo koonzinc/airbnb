a12d56a0d5a140f4065325ae896fb3671c76e9bf
koonzinc <koonz.eth@gmail.com>
2022-11-01 09:38:35 -0400

    fix sizing large card

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,43 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14  bg-gray-100 text-gray-600'>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a great clone though</p>
+        <p>Referrals accepted</p>
+        <p>William Koonz</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className="font-bold">HOST</h5>
+        <p>William Koonz</p>
+        <p>Presents</p>
+        <p>Airbnb clone</p>
+        <p>Great site</p>
+        <p>Enjoy your stay</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Say hi</p>
+        <p>Great Footer</p>
+        <p>Right here</p>
+      </div>
+      <div>
+        <h5 className="font-bold"></h5>
+      </div>
+    </div>
   )
 }
 
--- components/LargeCard.js
+++ components/LargeCard.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
-const LargeCard = ({img, title, description, buttonText}) => {
+const LargeCard = ({ img, title, description, buttonText }) => {
   return (
-    <section className='relative py-16 cursor-pointer'>
-        <div className='relative h-96 min-w-[300px]'>
-            <Image src={img} width={6000} height={2000} className="object-cover rounded-2xl" />
-        </div>
-        <div className='absolute top-24 left-8 md:top-32 md:left-12'>
-            <h3 className='text-2xl md:text-4xl mb-3 w-64'>{title}</h3>
-            <p>{description}</p>
+    <section className="relative py-16 cursor-pointer">
+      <div className="relative min-h-96 min-w-[300px]">
+        <Image
+          layout="fill"
+          src={img}
+          width={6000}
+          height={2000}
+          className="object-cover rounded-2xl"
+        />
+      </div>
+      <div className="absolute top-24 left-8 md:top-32 md:left-12">
+        <h3 className="text-2xl md:text-4xl mb-3 w-64">{title}</h3>
+        <p>{description}</p>
 
-            <button className='text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5'>{buttonText}</button>
-        </div>
+        <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5">
+          {buttonText}
+        </button>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default LargeCard
+export default LargeCard;
